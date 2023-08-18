cfce5d8adf452695029898b3d5c0e2124e803baf
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-17 23:18:11 -0400

    added movie genres to Genres

--- src/components/Genres/Genres.jsx
+++ src/components/Genres/Genres.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function Genres() {
+export default function Genres({ genreIds }) {
   const [genres, setGenres] = useState([]);
 
   useEffect(() => {
@@ -12,12 +12,28 @@ function Genres() {
         }&language=en`
       )
       .then((res) => {
-        console.log("res from genres");
-        console.log(res);
+        setGenres(res.data.genres);
       });
   });
 
-  return <div>Genres</div>;
+  return (
+    <div className="genre-container">
+      <p>Genres:&nbsp;</p>
+      {/* maps through the genreIds prop passed at top */}
+      {/* has a second param for index to add comma & space */}
+      {genreIds.map((id, index) => {
+        for (let i = 0; i < genres.length; i++) {
+          if (genres[i].id === id) {
+            return (
+              <p key={id}>
+                {genres[i]?.name}
+                {/* length - 1 b/c starts at 0, adds comma and space */}
+                {index === genreIds.length - 1 ? "" : ","}&nbsp;
+              </p>
+            );
+          }
+        }
+      })}
+    </div>
+  );
 }
-
-export default Genres;
