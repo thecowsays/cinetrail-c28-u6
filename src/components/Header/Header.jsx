24f56b2c497d2ee1f8e7fc3a949e284992b03435
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-08-19 22:21:37 -0400

    updated Header and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
+
+import "./Header.css";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header-container">
+      <Link className="logo" to="/">
+        Cinetrail
+      </Link>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search movies..."
+          className="search-input"
+        />
+      </div>
+      <div className="header-buttons-container">
+        <div className="theme-buttons-container">
+          <MdOutlineLightMode className="theme-icon" />
+          <MdOutlineDarkMode className="theme-icon theme-icon-active" />
+        </div>
+        <button className="create-account-btn">Create an account</button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
